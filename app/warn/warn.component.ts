3047223e5879bef0afd6aaf459e9a18f3d44a124
Pryn1k-IT <78645474+Pryn1k-IT@users.noreply.github.com>
2022-06-09 13:10:45 +0300

    First-load

---
+++ app/warn/warn.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-warn',
+  templateUrl: './warn.component.html',
+  styleUrls: ['./warn.component.scss']
+})
+export class WarnComponent implements OnInit {
+
+  @Output() onRemove = new EventEmitter()
+
+  @ViewChild('warn') private warnBlock!: ElementRef
+
+  removeWarn() {
+    this.warnBlock.nativeElement.remove();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
